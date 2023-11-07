8ce48afe9ae82a2b0aadbd7d871a174ec2b2c3fc
Damariz Licea <A01369045@itesm.mx>
2023-11-06 21:53:29 -0600

    Listo la doc

--- lab1/lab1/Assets/Scripts/PlayerController.cs
+++ lab1/lab1/Assets/Scripts/PlayerController.cs
@@ -1,50 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// This player controller class will update the events from the vehicle player.
 /// Standar coding documentarion can be found in 
 /// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/documentation-comments
 /// </summary>
+
 public class PlayerController : MonoBehaviour
-{
+{   
+
+
+    // Variables de movimiento del jugador
+    // <value> speed: Velocidad del vehículo </value>
+    // <value> turnSpeed: Velocidad de giro del vehículo </value>
     public float speed = 5.0f;
     public float turnSpeed = 0.0f;
     public float horizontalInput;
     public float forwardInput;
 
-    /// <summary>
-    //Variables cámara 
-    /// </summary>
+    //Variabkes de la cámara
+
     public Camera mainCamera;
     public Camera hoodCamera;
-    public KeyCode switchKey; //Tecla que permite cambiar entre cámaras
-    //Variables multijugador
+    public KeyCode switchKey;
     public string inputId;
-    /// <summary>
-    /// This method is called before the first frame update
-    /// </summary>
-    void Start()
-    {
-        
-    }
-    /// <summary>
-    /// This method is called once per frame
-    /// </summary>
-    void Update()
-{
-    horizontalInput = Input.GetAxis("Horizontal"+inputId);
-    forwardInput = Input.GetAxis("Vertical"+inputId);
 
-    transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-    transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    /// <summary> Metodo Start: Se ejecuta una vez al inicio del juego </summary>
 
-    //Cambio entre cámaras
-    if(Input.GetKeyDown(switchKey))
+    private void Start()
     {
-        mainCamera.enabled = !mainCamera.enabled;
-        hoodCamera.enabled = !hoodCamera.enabled;
     }
-}
+
+    /// <summary> Metodo Update: Se ejecuta una vez por frame </summary>
+
+    private void Update()
+    {
+        // Movimiento del vehículo según el id del jugador, partir el teclado.
+        horizontalInput = Input.GetAxis("Horizontal" + inputId);
+        forwardInput = Input.GetAxis("Vertical" + inputId);
+
+        // Movimiento del vehículo adelante y rotación
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        // Cambio entre cámaras
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+    }
+
 }
